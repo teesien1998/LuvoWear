3ee68131fb0485f1d28c9f706f1624dc4dfe6299
teesien <zeunesse@hotmail.com>
2025-07-21 23:20:34 +1000

    Use secrets for build arguments

--- frontend/src/redux/api/apiSlice.js
+++ frontend/src/redux/api/apiSlice.js
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const baseUrl = import.meta.env.VITE_BACKEND_URL || "http://localhost:8000";
-
+console.log("Base URL:", baseUrl);
 export const apiSlice = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({

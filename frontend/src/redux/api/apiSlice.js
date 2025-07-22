2b04ef8542fdbf93876e68150cdb54adfba81145
teesien <zeunesse@hotmail.com>
2025-07-22 18:11:47 +1000

    Updated the docker frontend map port 3000

--- frontend/src/redux/api/apiSlice.js
+++ frontend/src/redux/api/apiSlice.js
@@ -2,6 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const baseUrl = import.meta.env.VITE_BACKEND_URL || "http://localhost:8000";
 console.log("Base URL:", baseUrl);
+
 export const apiSlice = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
